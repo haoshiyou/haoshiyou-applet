74aab46d01d652b4d55ba207d7e290819f715a75
Lawrence Zhu <lawrencezjh@gmail.com>
2019-06-16 16:29:52 -0700

    Fullscreen preview image when click on details swiper images .

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -1,10 +1,25 @@
 Page({
   data: {
     listing: {},
+    imageURLs: {},
   },
   onLoad: function (options) {
     this.setData({
       listing: JSON.parse(decodeURIComponent(options.listing))
     });
+    var baseURL = 'http://res.cloudinary.com/xinbenlv/image/upload/c_fill,g_north,w_750,h_450,g_center/';
+    var urls = [];
+    for (let i = 0; i < this.data.listing.imageIds.length; ++i) {
+      urls.push(baseURL + this.data.listing.imageIds[i] + '.jpg');
+    }
+    this.setData({
+      imageURLs: urls
+    });
+  },
+  swiperClick: function (event) {
+    wx.previewImage({
+      current: event.target.dataset.imageURL,
+      urls: this.data.imageURLs
+    })
   },
 })
